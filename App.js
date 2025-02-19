a4e302c5524769965802c8d740ee2dc7876d7910
Yash777Salave <yashsalave777@rediffmail.com>
2025-02-19 15:39:29 +0530

    created product card

---
+++ App.js
@@ -0,0 +1,11 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import MyProducts from './src/screens/MyProducts';
+
+const App = () => {
+  return <MyProducts />;
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
---
+++ src/screens/MyProducts.js
@@ -0,0 +1,113 @@
+import {
+  FlatList,
+  Image,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import React from 'react';
+import {products} from '../componant/Products';
+const MyProducts = () => {
+  return (
+    <View style={styles.MainContainer}>
+      <FlatList
+        data={products}
+        renderItem={({item, index}) => (
+          <View style={styles.ProductCardContainer}>
+            <View>
+              <Image source={{uri: item.image}} style={styles.Images} />
+            </View>
+            <View style={{padding: 10}}>
+              <Text style={styles.productTXT}>
+                {item.name.substring(0, 25) + '...'}
+              </Text>
+              <Text style={styles.brandTXT}>{item.brand}</Text>
+              <Text style={[styles.brandTXT, {color: 'green', fontSize: 16}]}>
+                {'₹' + item.price}
+              </Text>
+              <View style={styles.MainPlusMinusCart}>
+                <View>
+                  <TouchableOpacity style={styles.cartButton}>
+                    <Text style={styles.cartButtonTXT}>Add To Cart</Text>
+                  </TouchableOpacity>
+                </View>
+                {item.qty > 0 && (
+                  <View style={{flexDirection: 'row'}}>
+                    <TouchableOpacity style={styles.cartButton}>
+                      <Text style={[styles.cartButtonTXT]}>-</Text>
+                    </TouchableOpacity>
+                    <Text style={styles.qty}>{'0'}</Text>
+                    <TouchableOpacity style={styles.cartButton}>
+                      <Text style={styles.cartButtonTXT}>+</Text>
+                    </TouchableOpacity>
+                  </View>
+                )}
+              </View>
+            </View>
+          </View>
+        )}
+      />
+    </View>
+  );
+};
+
+export default MyProducts;
+
+const styles = StyleSheet.create({
+  MainContainer: {
+    flex: 1,
+  },
+  ProductCardContainer: {
+    // borderWidth: 1,
+    height: 120,
+    width: '94%',
+    alignSelf: 'center',
+    margin: 10,
+    backgroundColor: '#ffff',
+    elevation: 5,
+    borderRadius: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  Images: {
+    height: 100,
+    width: 100,
+  },
+  productTXT: {
+    fontSize: 15,
+    // width: 200,
+  },
+  brandTXT: {
+    fontSize: 14,
+    width: 200,
+    color: '#A8A8A8',
+    fontWeight: 'bold',
+  },
+  cartButton: {
+    paddingLeft: 10,
+    paddingRight: 10,
+    height: 29,
+    backgroundColor: 'green',
+    justifyContent: 'center',
+    alignItems: 'center',
+    // width:
+    margin: 10,
+    marginTop: 10,
+    borderRadius: 8,
+  },
+  cartButtonTXT: {
+    fontSize: 15,
+    color: '#ffff',
+  },
+  MainPlusMinusCart: {
+    flexDirection: 'row',
+  },
+  qty: {
+    padding: 5,
+    alignItems: 'center',
+    alignSelf: 'center',
+    fontSize: 16,
+    alignContent: 'center',
+  },
+});
